4df1710093f654e7b00dcf70955bcb10948436ef
YesUzb <raxmonovdaler6@gmail.com>
2022-08-06 13:50:51 +0500

    Xarajatlar hisoblagichi

---
+++ js/main.js
@@ -0,0 +1,12 @@
+alert("Xush kelibsiz")
+var passBound = 9556983.2;Xara 
+var number = +prompt ("Sayohatga borishga qancha pulingiz bor yozing!")
+var elText = document.querySelector(".js-text")
+
+if (passBound <= number) {
+    console.log("Sayohat uchun yetarlicha pulingiz bor mazza qilib dam olib keling.")
+    elText.textContent = "Sayohat uchun yetarlicha pulingiz bor mazza qilib dam olib keling"
+} else {
+    console.log("Kechirasiz sizda yetarlicha mablag'ingiz yo'q Xohlasangiz shu pul bilan shuyerda ham dam olishingiz mumkin.")
+    elText.textContent = "Kechirasiz sizda yetarlicha mablag'ingiz yo'q Xohlasangiz shu pul bilan shuyerda ham dam olishingiz mumkin."
+}
